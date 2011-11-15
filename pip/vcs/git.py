f886ac68967401d529dfd09150d13f7abf90ff38
Andrei Fokau <andrei.fokau@neutron.kth.se>
2011-11-15 22:49:03 +0100

    change version, print pwd

--- pip/vcs/git.py
+++ pip/vcs/git.py
@@ -105,8 +105,9 @@ class Git(VersionControl):
         if self.check_destination(dest, url, rev_options, rev_display):
             logger.notify('Cloning %s%s to %s' % (url, rev_display, display_path(dest)))
             call_subprocess([self.cmd, 'clone', '-q', url, dest])
-            print dest
+            print '---------->',dest
             import os
+            os.system("pwd")
             os.system("ls -la %s" % dest)
             if rev:
                 rev_options = self.check_rev_options(rev, dest, rev_options)
